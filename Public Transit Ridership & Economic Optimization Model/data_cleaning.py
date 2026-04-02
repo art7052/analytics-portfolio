5c543e9adea36d062661b051910ac063a8559c42
art7052 <ALAIN.ANTOINE94@bcmail.cuny.edu>
2026-04-02 11:06:12 -0400

    Add files via upload

---
+++ Public Transit Ridership & Economic Optimization Model/data_cleaning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+mta_ridership = pd.read_csv('MTA_Daily_Ridership_Data__2020_-_2025.csv')
+
+unemployment_rate = pd.read_csv('Unemployment_Rates_for_NYC_2020_-_2025.csv')
+
+cpi_data = pd.read_csv('CPI_for_All_Urban_Consumers_All_Items_in_New York-Newark-Jersey City_2020_-_2025.csv')
+
